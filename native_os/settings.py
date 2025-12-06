c028ee624058a4620500f7287dab2eb29ae5dcdc
Mohammed Charaf Amri <charafamri111@email.com>
2025-12-06 21:41:57 +0100

    Simplify CORS: accept all origins

--- native_os/settings.py
+++ native_os/settings.py
@@ -163,8 +163,8 @@ REST_FRAMEWORK = {
     }
 }
 
-# CORS
-CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:3000,http://127.0.0.1:3000,http://localhost:8080,http://127.0.0.1:8080,http://localhost:8081,http://127.0.0.1:8081', cast=lambda v: [s.strip() for s in v.split(',')])
+# CORS - Allow all origins for simplicity
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 # Security settings for production
